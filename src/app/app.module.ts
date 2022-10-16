337a671be2f4d1602a12c0bcdefbe45c7c09ff9e
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-10-16 20:44:09 -0300

    componentes,login,registro,navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,18 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
+import { MaterialModule } from './material/material.module';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +30,7 @@ import { HomeComponent } from './pages/home/home.component';
     AngularFireModule.initializeApp(environment.firebase),
     NgxSpinnerModule,
     BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
