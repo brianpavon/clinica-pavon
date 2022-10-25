ad5515fd0d5207bb7d18e89e5343fe5687ac365f
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-10-24 23:19:15 -0300

    modificaciones extra primer sprint ok

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { Usuarios } from 'src/app/interfaces/usuarios';
 import { AuthService } from 'src/app/services/auth.service';
+import { ImagenService } from 'src/app/services/imagen.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +12,22 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usuarioVerificado : boolean = false;
+  esAdmin : boolean = false;
+  listaUsuarios : Usuarios[] = [];
+  pacientes : number = 3;
+  medicos : number = 2;
 
-  constructor(private spinner: NgxSpinnerService,private auth :AuthService,private routes : Router) { }
+  constructor(private spinner: NgxSpinnerService,private auth :AuthService,private routes : Router,private userServ : UsuariosService, private imgServ : ImagenService) { }
 
   ngOnInit(): void {
+    this.spinner.show();
+  
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 1500);    
+    this.cargarIngresoRapido();
   }
 
   redirectToHome() {
@@ -24,13 +39,44 @@ export class LoginComponent implements OnInit {
       
       await this.auth.login(email, password);      
       
-      // this.listaUsuarios  = [];
-      // this.pacientes  = 3;
-      // this.medicos  = 2;  
+      this.listaUsuarios  = [];
+      this.pacientes  = 3;
+      this.medicos  = 2;  
       
     } catch (error: any) {
       this.auth.thrownErrorsLogin(error.code);
     }
+  }
+
+  cargarIngresoRapido(){    
+    this.userServ.traerUsuarios().subscribe(
+      usuarios =>{
+        //console.log(usuarios);
+        usuarios.forEach(usuario => {          
+          if(usuario.email == "admin@mail.com" && this.listaUsuarios.length < 6){
+            this.listaUsuarios.push(usuario);
+          }
+          //el paciente tiene 2 fotos, asi que tomo la primera
+          if(usuario.rol == 'paciente' && this.pacientes > 0){
+            //usuario.fotos = usuario.fotos.split(',')[0];
+            this.listaUsuarios.push(usuario);
+            this.pacientes--;
+          }
+          if(usuario.rol == 'medico' && this.medicos>0 && usuario.email != 'rfrrdfd@dd.dd'){
+            this.listaUsuarios.push(usuario);
+            this.medicos--;
+          }
+          
+          this.imgServ.descargarImagen(usuario.fotoPerfil).subscribe(
+            url =>{
+              usuario.fotoPerfil = url;
+              //console.log(url);
+            }
+          )
+        });
+        
+      }
+    )
   }
 
 }
