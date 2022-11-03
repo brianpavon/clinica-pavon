9090f7074f3517f84da32ec7a2e34b99deb040e0
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-11-03 19:25:48 -0300

    agrego turnos especialista

--- src/app/turnos/turnos-medico/turnos-medico.component.ts
+++ src/app/turnos/turnos-medico/turnos-medico.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Turnos } from 'src/app/interfaces/turnos';
+import { Usuarios } from 'src/app/interfaces/usuarios';
+import { AuthService } from 'src/app/services/auth.service';
+import { TurnosService } from 'src/app/services/turnos.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-turnos-medico',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurnosMedicoComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['paciente', 'especialidad', 'fecha','hora','estado','acciones'];
+
+  turnosMedico : Turnos[] = [];
+  medicoLogueado !: Usuarios | undefined;
+
+  constructor(private auth:AuthService,private userServ : UsuariosService,private turnServ : TurnosService) { }
 
   ngOnInit(): void {
+    this.cargarTurnosMedico()
+  }
+
+  cargarTurnosMedico(){
+    
+    this.auth.obtenerUsuarioLogueado().pipe(
+      mergeMap(async res1 => this.medicoLogueado = await this.userServ.devolverDataUsuarioDB(res1?.uid))
+    ).subscribe(data =>{
+      this.turnServ.traerTurnos().subscribe(turnos => {
+        //console.log(turnos);
+        
+        this.turnosMedico = turnos.filter(t => t.medico.id == data?.id)
+        //console.log(this.turnosPaciente);
+        
+      })
+    })
   }
 
 }
