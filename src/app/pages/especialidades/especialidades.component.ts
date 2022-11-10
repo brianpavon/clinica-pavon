2850a2de768946802553fbae5fd5424602fe7e8f
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-11-10 00:36:15 -0300

    agrego imagen al alta de especialidad

--- src/app/pages/especialidades/especialidades.component.ts
+++ src/app/pages/especialidades/especialidades.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Especialidades } from 'src/app/interfaces/especialidades';
 import { EspecialidadesService } from 'src/app/services/especialidades.service';
+import { ImagenService } from 'src/app/services/imagen.service';
 
 
 @Component({
@@ -14,8 +15,10 @@ export class EspecialidadesComponent implements OnInit {
   formEspecialidad:FormGroup;
   especialidadNueva !: Especialidades;
   @Output() especialidadSeleccionada : EventEmitter<any> = new EventEmitter<any>();
+  imgEspec !: File;
+  urlImgEsp : string = '';
 
-  constructor(private servEsp : EspecialidadesService, private fb : FormBuilder) {
+  constructor(private servEsp : EspecialidadesService, private fb : FormBuilder,private imgServ : ImagenService) {
     this.formEspecialidad = this.fb.group(
       {
         'descripcion':['',[Validators.required,Validators.minLength(5)]]
@@ -31,16 +34,27 @@ export class EspecialidadesComponent implements OnInit {
   cargarEspecialidades(){
     this.servEsp.traerTodasLasEspecialidades().subscribe(
       esp => {
-        this.listaEspecialidades = esp;        
+        this.listaEspecialidades = esp; 
+        this.listaEspecialidades.forEach(element => {
+          element.urlFoto != '' ? element.urlFoto = element.urlFoto : element.urlFoto = 'medicine.png'          
+          this.imgServ.descargarImagen(element.urlFoto).subscribe(
+            url => {
+              element.urlFoto = url;
+            }
+          )
+          
+        });   
       }
     )
   }
 
   async nuevaEspecialidad(){
     this.especialidadNueva = this.formEspecialidad.value;
+    this.especialidadNueva.urlFoto = this.urlImgEsp;
     
     //console.log(this.especialidadNueva);
     this.servEsp.guardarEspecialidad(this.especialidadNueva);
+    if(this.urlImgEsp != "") this.imgServ.guardarImagen(this.imgEspec,this.urlImgEsp);
    
     this.formEspecialidad.reset();    
     //@ts-ignore
@@ -55,5 +69,15 @@ export class EspecialidadesComponent implements OnInit {
     
     this.especialidadSeleccionada.emit(event);
   }
+
+  resetear(){
+    this.formEspecialidad.reset();
+  }
+
+  cargarImagen(event : any){
+    const fileImg : File = event.target.files[0];
+    this.imgEspec = fileImg;      
+    this.urlImgEsp = event.target.files[0].name;
+  }  
 
 }
